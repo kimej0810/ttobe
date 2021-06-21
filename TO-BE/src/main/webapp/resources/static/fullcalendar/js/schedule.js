1b603e3ec940ab1874edacaca719780830102dd9
kimej0810 <56124293+kimej0810@users.noreply.github.com>
2021-06-21 11:31:38 +0900

    Delete TO-BE/src/main/webapp/resources/static/fullcalendar directory
    ㅁ

--- TO-BE/src/main/webapp/resources/static/fullcalendar/js/schedule.js
+++
@@ -1,8 +0,0 @@
-// add schedule popup open
-function click_add() {
-	var url = "schedulePopup";
-	var name = "schedulePopup";
-	var option = "width = 600, height = 600 left = 100, top=50,location=no";
-	window.open(url,name,option)
-};
-
