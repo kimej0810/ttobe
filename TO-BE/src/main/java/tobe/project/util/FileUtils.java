a294661b8ef1723f7f56ffe6269b52ca7c0ba31c
kimej0810 <56124293+kimej0810@users.noreply.github.com>
2021-06-21 13:38:56 +0900

    ``

--- TO-BE/src/main/java/tobe/project/util/FileUtils.java
+++ TO-BE/src/main/java/tobe/project/util/FileUtils.java
@@ -21,10 +21,11 @@ import tobe.project.dto.MemberVO;
 @Component("fileUtils")
 public class FileUtils {
 	//파일 저장될 경로
+
+	//private static final String filePath = "D:\\kio\\tobe\\TO-BE\\src\\main\\webapp\\resources\\static\\profile\\";
+	private static final String filePath = "C:\\git\\ttobe\\TO-BE\\src\\main\\webapp\\resources\\static\\file\\"; // 파일이 저장될 위치 수진
 	//private static final String filePath = "D:\\kio\\tobe\\TO-BE\\src\\main\\webapp\\resources\\static\\profile\\";
 	//private static final String filePath = "C:\\api_dev\\to-be3\\TO-BE\\src\\main\\webapp\\resources\\static\\file\\"; // 파일이 저장될 위치 수진
-	private static final String filePath = "C:\\Users\\750\\git\\ttobe\\TO-BE\\src\\main\\webapp\\resources\\static\\profile\\";
-	
 	
 	public List<Map<String, Object>> parseInsertFileInfoProfile(MemberVO memberVO, MultipartHttpServletRequest mpRequest) throws IllegalStateException, IOException{
 		Iterator<String> iterator = mpRequest.getFileNames();
