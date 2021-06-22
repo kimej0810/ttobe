ea4f76bcb65583b554c338fe11c114292c20254b
K-RG <78725070+K-RG@users.noreply.github.com>
2021-06-22 13:51:31 +0900

    Add files via upload
    oo

--- TO-BE/src/main/webapp/resources/static/fullcalendar/js/schedulepop.js
+++ TO-BE/src/main/webapp/resources/static/fullcalendar/js/schedulepop.js
@@ -1,24 +1,3 @@
-//datepicker
-/*$(function() {
-	$.datepicker.setDefaults({
-		dateFormat : 'yy-mm-dd',
-		showOtherMonths : true,
-		showMonthAfterYear : true,
-		changeYear : true,
-		changeMonth : true,          
-       yearSuffix: "년",
-      	monthNamesShort: ['1','2','3','4','5','6','7','8','9','10','11','12'],
-      	monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'],
-       dayNamesMin: ['일','월','화','수','목','금','토'],
-       dayNames: ['일요일','월요일','화요일','수요일','목요일','금요일','토요일']
-	});
-	$("#s_startDate").datepicker();
-	$("#s_endDate").datepicker();
-	
-	$("#s_startDate").datepicker('setDate', 'today');
-	$("#s_endDate").datepicker('setDate', '+1d');
-});
-*/
 
 //add schedule
 $.fn.serializeObject = function(){
