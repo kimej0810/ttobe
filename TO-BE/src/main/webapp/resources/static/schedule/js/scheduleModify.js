f6717e207b86102c6a5b840373940011a9753427
K-RG <78725070+K-RG@users.noreply.github.com>
2021-07-15 21:56:31 +0900

    Update scheduleModify.js
    날짜선택 수정 ! 확인은 학원에서!

--- TO-BE/src/main/webapp/resources/static/schedule/js/scheduleModify.js
+++ TO-BE/src/main/webapp/resources/static/schedule/js/scheduleModify.js
@@ -1,9 +1,30 @@
 $(document).ready(function(){
-	$("#s_startDate, #s_endDate").datetimepicker({
+	$("#s_startDate").datetimepicker({
+		dateFormat:"yyyy-mm-dd hh:mm",
+		minDate:'+1d',
+		minTime:'+1h',
+		onChangeDateTime:function(){
+			if($("#s_startDate").val() > $("#s_endDate").val()){
+				alert("시작일을 올바르게 선택해주세요.");
+				$("#s_startDate").val($("#s_endDate").val());
+			}
+		},
+		disabledWeekDays :[0, 6]
+	});
+	$("#s_endDate").datetimepicker({
+		dateFormat:"yyyy-mm-dd hh:mm",
+		minDate:'+1d',
+		minTime:'+1h',
+		onChangeDateTime:function(){
+			if($("#s_startDate").val() > $("#s_endDate").val()){
+				alert("시작일을 올바르게 선택해주세요.");
+				$("#s_endDate").val($("#s_startDate").val());
+			}
+		},
 		disabledWeekDays :[0, 6]
 	});
 	jQuery.datetimepicker.setLocale('kr');
-})
+});
 $.fn.serializeObject = function(){
     var o = {};
     var a = this.serializeArray();
