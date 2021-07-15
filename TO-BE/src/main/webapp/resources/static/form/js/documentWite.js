e326aacb2c98281d596420ed37ec4fe7a13c01bb
K-RG <78725070+K-RG@users.noreply.github.com>
2021-07-15 22:03:14 +0900

    날짜선택 수정! 확인은 학원에서....

--- TO-BE/src/main/webapp/resources/static/form/js/documentWite.js
+++ TO-BE/src/main/webapp/resources/static/form/js/documentWite.js
@@ -1,6 +1,42 @@
 $(document).ready(function(){
-	$("#e_draftDate, #e_startDay").datetimepicker({
+	$("#e_draftDate").datetimepicker({
 		disabledWeekDays :[0, 6],
+		dateFormat:"yyyy-mm-dd hh:mm",
+		minDate:'+1d',
+		minTime:'+1h',
+		onChangeDateTime:function(){
+			if($("#e_draftDate").val() > $("#e_startDay").val()){
+				alert("기안일자를 올바르게 선택해주세요.");
+				$("#e_draftDate").val($("#e_startDay").val());
+			}
+		},
+		i18n:{
+			de:{
+				months:[
+					'Januar','Februar','März','April',
+					'Mai','Juni','Juli','August',
+					'September','Oktober','November','Dezember',
+				],
+				dayOfWeek:[
+					"So.", "Mo", "Di", "Mi", 
+					"Do", "Fr", "Sa.",
+				]
+			}
+		},
+		timepicker:false,
+		format:'Y-m-d'
+	});
+	$("#e_startDay").datetimepicker({
+		disabledWeekDays :[0, 6],
+		dateFormat:"yyyy-mm-dd hh:mm",
+		minDate:'+1d',
+		minTime:'+1h',
+		onChangeDateTime:function(){
+			if($("#e_startDay").val() > $("#e_draftDate").val()){
+				alert("시행일자를 올바르게 선택해주세요.");
+				$("#e_startDay").val($("#e_draftDate").val());
+			}
+		},
 		i18n:{
 			de:{
 				months:[
@@ -19,9 +55,6 @@ $(document).ready(function(){
 	});
 	jQuery.datetimepicker.setLocale('kr');
 });
-	
-
-
 jQuery.fn.serializeObject = function(){
 	var o = {};
 	var a = this.serializeArray();
